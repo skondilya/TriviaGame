469e064d053219a9d72bfc822ad10bdce6edfacd
skondilya <skondilya@gmail.com>
2017-03-03 16:33:32 -0600

    adding the buttonsfor each question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,91 +2,61 @@
 var questions = {
   q1:{
       question:"Which game is played with five players on either side?",
-      answer1: "Basketball",
-      answer2: "Volleyball",
-      answer3: "Hockey",
-      answer4: "Football",
+      choices: ["Basketball","Volleyball","Hockey","Football"],
       correctAnswer: "Basketball",
       },
 
   q2:{
       question:"Which of the following countries is landlocked?",
-      answer1: "Switzerland",
-      answer2: "Italy",
-      answer3: "Spain",
-      answer4: "France",
+      choices: ["Switzerland","Italy","Spain","France"],
       correctAnswer: "Switzerland",
       }, 
   q3:{
       question:".MPG' extension refers usually to what kind of file?",
-      answer1:"WordPerfect Document file",
-      answer2:"MS Office document",
-      answer3:"Animation/movie file",
-      answer4:"Image file",
+      choices: ["WordPerfect Document file","MS Office document","Animation/movie file","Image file"],
       correctAnswer:"Animation/movie file",
       }, 
       
   q4:{
       question:"Which country is ruled by a single dynasty for more than two thousand years?",
-      answer1:"England",
-      answer2:"Persia",
-      answer3:"Japan",
-      answer4:" Egypt",
+      choices: ["England","Persia","Japan"," Egypt"],
       correctAnswer:"Japan",
       }, 
       
   q5:{
       question:"In which Disney film would you find a rabbit named Thumper?",
-      answer1:"Bambi",
-      answer2:"Peter Pan",
-      answer3:"Hercules",
-      answer4:"Frozen",
+      choices: ["Bambi","Peter Pan","Hercules","Frozen"],
       correctAnswer: "Bambi",
       }, 
 
   q6:{
       question:"What colour is Cerulean?",
-      answer1:"Red",
-      answer2:"Blue",
-      answer3:"Yellow",
-      answer4:"Green",
+      choices: ["Red","Blue","Yellow","Green"],
       correctAnswer: "Blue",
       }, 
 
   q7:{
       question:"What are a group of Dolphins called?",
-      answer1:"School",
-      answer2:"Herd",
-      answer3:"Pod",
-      answer4:"Pool",
+      choices: ["School","Herd","Pod","Pool"],
       correctAnswer: "Pod",
       }, 
 
      
   q8:{
       question:" What is the currency of Brazil?",
-      answer1:"Dollar",
-      answer2:"The Bhat",
-      answer3:"Real",
-      answer4:"Pool",
+      choices: ["Dollar","The Bhat","Real","Pool"],
       correctAnswer: "Krona",
       }, 
 
   q9:{
       question:"Who has won the most Academy Awards?",
-      answer1:"James Cameron",
-      answer2:"Walt Disney",
-      answer3:"Katherine Hepburn",
-      answer4:"Steven Spielberg",
+      choices: ["James Cameron","Walt Disney","Katherine Hepburn","Steven Spielberg"],
       correctAnswer: "Walt Disney",
       }, 
 
   q10:{
       question:"Which of the following is not a gas?",
-      answer1:"Nitrogen",
-      answer2:"Oxygen",
-      answer3:"Helium",
-      answer4:"Mercury",
+      choices: ["Nitrogen","Oxygen","Helium","Mercury"],
       correctAnswer:"Mercury",
       }
     }       
@@ -122,35 +92,44 @@ var questions = {
          return;
       }
 
-     document.getElementById("timer").innerHTML=count + " secs"; // watch for spelling
+    // document.getElementById("timer").innerHTML=count + " secs"; // watch for spelling
     } 
   }
 
   // the function utilizes “this”, and for loop to append to page
   function loadQuestion(){
   var objKeys = Object.keys(questions);
-  console.log(objKeys);
-  var timer = $("<h2>Time Remaining</h2>");
+  //console.log(objKeys);
+  var timer = $("<h2>Time Remaining :</h2>");
   for (var i = 0; i < objKeys.length; i++) {
-  var qDiv =  $("<div>")
-  var questionDiv = $("<div>").text(questions[objKeys[i]].question)
-  console.log(objKeys[i]);
-  console.log(questions[objKeys[i]]);
-  console.log(questions[objKeys[i]].answer1);
-  var a1Div = $("<div class='answer'>").text(questions[objKeys[i]].answer1)
-  var a2Div = $("<div class='answer'>").text(questions[objKeys[i]].answer2)
-  var a3Div = $("<div class='answer'>").text(questions[objKeys[i]].answer3)
-  var a4Div = $("<div class='answer'>").text(questions[objKeys[i]].answer4)
-  $(".loadQuestion").append(qDiv).append(questionDiv).append(a1Div).append(a2Div).append(a3Div).append(a4Div);
+  var qDiv =  $("<div>");
+  var questionDiv = $("<div>").text(questions[objKeys[i]].question);
+  $(".loadQuestion").append(qDiv).append(questionDiv);
   $("#start").hide();
-  if (countDownTimer()==0){
-  objKeys++;
-  }
-  }
-  var radioBtn = $('<input type="radio" name="rbtnCount" />');
-  $(".answer").prepend(radioBtn);
-  $(".loadQuestion").prepend(timer);
+  var radioButtons = createRadios(i);
+  questionDiv.append(radioButtons);
   } 
+  $(".loadQuestion").prepend(timer);
+  }
+
+// Creates a list of the answer choices as radio inputs
+function createRadios(index) {
+    var radioList = $('<ul>');
+    var item;
+    var input = '';
+    var objKeys = Object.keys(questions);
+    console.log(questions[objKeys[index]].choices);
+    for (var i = 0; i < questions[objKeys[index]].choices.length; i++) {
+      item = $('<li>');
+      input = '<input type="radio" name="answer'+index+'" value=' + i + ' />';
+      input += questions[objKeys[index]].choices[i];
+      item.append(input);
+      radioList.append(item);
+    }
+    return radioList;
+} 
+
+
 
   // Change from current questions to next question ++
   function nextQuestion(){
@@ -176,4 +155,4 @@ $(document).ready(function () {
   $("#start").on("click", function(){
     loadQuestion() ; 
   });  
-})
+});
